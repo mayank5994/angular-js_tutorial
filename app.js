18ea57dec9e27c78d8ed0983cc0026a3f0339d2d
Mayank Agarwal <mayankagarwal5994@gmail.com>
2016-12-24 18:18:34 +0530

    First Assignment

---
+++ app.js
@@ -0,0 +1,48 @@
+(function() {
+   'use strict';
+
+   angular.module("LunchCheck",[])
+
+   .controller('LunchFormController',LunchFormController);
+
+
+   LunchFormController.$inject='$scope'];
+
+
+   function LunchFormController($scope){
+      $scope.lunches="";
+      $scope.message="";
+      $scope.customStyle="";
+
+
+      $scope.displayMessage= function() {
+          var count= countLunches($scope.lunches);
+          if(!count)
+          {
+              $scope.message="Please enter data first";
+              $scope.customStyle = {'color': 'red', 'border':'1px solid red'};
+          }
+          else
+          {
+            if(count && count<=3)
+            {
+              $scope.message="Enjoy";
+            }
+            else if (count > 3)
+            {
+                $scope.message = "Too much!";
+            }
+            $scope.customStyle = {'color': 'green', 'border':'1px solid green'};
+          }
+        };
+
+        var countLunches= function (str){
+          if(str.length==0)
+              return 0;
+          else {
+            var lunchArray= str.split(',');
+            return lunchArray.length;
+          }
+        };
+      }   
+})();
