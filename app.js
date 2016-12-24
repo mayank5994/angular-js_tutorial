b62e6d0ce814356635683f781b8cf5b96976eb7c
Mayank Agarwal <mayankagarwal5994@gmail.com>
2016-12-24 18:54:27 +0530

    first

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
-(function() {
-   'use strict';
+(function () {
+  'use strict';
 
-   angular.module("LunchCheck",[])
+  angular.module("LunchCheck", [])
 
-   .controller('LunchFormController',LunchFormController);
+  .controller('LunchFormController', LunchFormController);
 
+  LunchFormController.$inject = ['$scope'];
+  function LunchFormController($scope) {
+    $scope.lunches = "";
+    $scope.message = "";
+    $scope.customStyle = "";
 
-   LunchFormController.$inject='$scope'];
+    $scope.displayMessage = function () {
+      var count = countLunches($scope.lunches);
+      if (!count) {
+        $scope.message = "Please enter data first";
+        $scope.customStyle = {'color': 'red', 'border':'1px solid red'};
+      } else {
+        if (count && count <= 3) {
+          $scope.message = "Enjoy!";
+        }
 
+        else if (count > 3) {
+          $scope.message = "Too much!";
+        }
 
-   function LunchFormController($scope){
-      $scope.lunches="";
-      $scope.message="";
-      $scope.customStyle="";
+        $scope.customStyle = {'color': 'green', 'border':'1px solid green'};
+      }
+    };
 
+    var countLunches = function (str) {
+      if(str.length === 0) return 0;
+      var lunchArray = str.split(',');
+      return lunchArray.length;
+    };
+  }
 
-      $scope.displayMessage= function() {
-          var count= countLunches($scope.lunches);
-          if(!count)
-          {
-              $scope.message="Please enter data first";
-              $scope.customStyle = {'color': 'red', 'border':'1px solid red'};
-          }
-          else
-          {
-            if(count && count<=3)
-            {
-              $scope.message="Enjoy";
-            }
-            else if (count > 3)
-            {
-                $scope.message = "Too much!";
-            }
-            $scope.customStyle = {'color': 'green', 'border':'1px solid green'};
-          }
-        };
-
-        var countLunches= function (str){
-          if(str.length==0)
-              return 0;
-          else {
-            var lunchArray= str.split(',');
-            return lunchArray.length;
-          }
-        };
-      }   
 })();
